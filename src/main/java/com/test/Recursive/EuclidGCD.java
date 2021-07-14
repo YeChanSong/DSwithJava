1792b10c16b36058ce9d995dbdc9b8f8fa360e88
YeChanSong <davidsyc1170@gmail.com>
2021-07-14 17:40:14 +0900

    test EuclidGCD.getArrayGCD

--- src/main/java/com/test/Recursive/EuclidGCD.java
+++ src/main/java/com/test/Recursive/EuclidGCD.java
@@ -3,11 +3,17 @@ package com.test.Recursive;
 public class EuclidGCD {
 
     public static int getGCD(int a, int b){
-        if(b == 0){
+        if(b == 0)
             return a;
-        }
-        else{
+        else
             return getGCD(b,a%b);
+    }
+
+    public static int getArrayGCD(int[] arr){
+        int gcd =arr[0];
+        for(int i =1;i<arr.length;i++){
+            gcd = getGCD(gcd,arr[i]);
         }
+        return gcd;
     }
 }
--- src/test/java/com/test/Recursive/EuclidGCDTest.java
+++ src/test/java/com/test/Recursive/EuclidGCDTest.java
@@ -12,4 +12,14 @@ public class EuclidGCDTest {
         assertThat(EuclidGCD.getGCD(b,a)).isEqualTo(2);
         assertThat(EuclidGCD.getGCD(3,7)).isEqualTo(1);
     }
+
+    @Test
+    public void getArrayGCD(){
+        int[] arr = {6,8,22,4,14};
+        int[] arr2 = {3,6,9,33};
+        int[] arr3 = {6,8,22,4,14,3,6,9,33};
+        assertThat(EuclidGCD.getArrayGCD(arr)).isEqualTo(2);
+        assertThat(EuclidGCD.getArrayGCD(arr2)).isEqualTo(3);
+        assertThat(EuclidGCD.getArrayGCD(arr3)).isEqualTo(1);
+    }
 }
